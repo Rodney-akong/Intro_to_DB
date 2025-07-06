1a0d85f122e18c763dd790c3e8cf71d380463acd
Akong Rodney <rodneyakong@gmail.com>
2025-07-06 18:24:53 +0100

    pushing all task at once

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,43 +1,42 @@
 
----Create database if not exist
-
+-- Create the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
----select the database
-
+-- Select the database
 USE alx_book_store;
 
----Create the authors table COMMENT
-CREATE TABLE authors(
+-- Create the AUTHORS table
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
-    );
+);
 
----create the books table
-CREATE TABLE Books(
+-- Create the BOOKS table
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY(author_id ) REFERENCES AUTHORS (author_id)
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
 );
 
----create costumer table
-CREATE TABLE customers (
+-- Create the CUSTOMERS table
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_address VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NUT NULL,
-    adress TEXT
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
 );
 
 -- Create the ORDERS table
-
-CREATE TABLE orders(
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+
+    
 );
 
 -- Create the ORDER_DETAILS table
